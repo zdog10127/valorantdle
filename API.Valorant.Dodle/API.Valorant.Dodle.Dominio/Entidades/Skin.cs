db04bd43c08761682c6ba2e810c9171c0debec1d
Zdog <49007962+zdog10127@users.noreply.github.com>
2023-11-27 08:56:03 -0300

    Initial Commit

---
+++ API.Valorant.Dodle/API.Valorant.Dodle.Dominio/Entidades/Skin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Valorantdle.Dominio.Entidades
+{
+    [Table("Skin")]
+    public class Skin
+    {
+        [Key]
+        [Required]
+        [Column("IdSkin", TypeName = "int")]
+        public int IdSkin { get; set; }
+
+        [Required]
+        [Column("IdArma", TypeName = "int")]
+        public int IdArma { get; set; }
+
+        [Column("Nome", TypeName = "varchar")]
+        [MaxLength(45)]
+        public string Nome { get; set; }
+
+        [Column("Pacote", TypeName = "varchar")]
+        [MaxLength(45)]
+        public string Pacote { get; set; }
+
+        [Column("ImagemSkin", TypeName = "varchar")]
+        [MaxLength(500)]
+        public string ImagemSkin { get; set; }
+
+        [Column("Rara", TypeName = "char")]
+        public bool Rara { get; set; }
+
+        [ForeignKey("IdArma")]
+        public Arsenal Arsenal { get; set; }
+    }
+}
---
+++ API.Valorant.Dodle/API.Valorant.Dodle.Dominio/Interfaces/Repositorio/IRepositorioArsenal.cs
@@ -0,0 +1,19 @@
+﻿using API.Valorantdle.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Valorantdle.Dominio.Interfaces.Repositorio
+{
+    public interface IRepositorioArsenal
+    {
+        Task<List<Arsenal>> ListarTodasAsArmasDoArsenal();
+        Task<List<Arsenal>> BuscarArmasPeloTipo(string tipo);
+        Task<Arsenal> BuscarPeloNome(string nome);
+        Task CadastrarArma(Arsenal arsenal);
+        Task AtualizarArma(int idMapas, Arsenal arsenal);
+        Task DeletarArma(int idArma);
+    }
+}
